3ae490bd8525a102e24e8ba5a8e7145e646ef90e
lafixier <lfxr@proton.me>
2023-02-19 21:17:56 +0900

    feat(UtliemCli): いい感じに調整

--- src/libs/utliem_cli.nim
+++ src/libs/utliem_cli.nim
@@ -1,21 +1,32 @@
-type UtliemCli* = object
+import
+  os
+
+
+type UtliemCli = object
   appDirectoryPath: string
 
 type UcImage = object
-  discard
+  utliemCli: ref UtliemCli
+  imagesDirPath: string
 
 type UcContainer = object
   discard
 
 
-proc newUtliemCli*(appDirectoryPath: string): UtliemCli =
+proc newUtliemCli*(appDirectoryPath: string): ref UtliemCli =
+  result = new UtliemCli
   result.appDirectoryPath = appDirectoryPath
 
-proc image*(uc: UtliemCli): UcImage =
-  discard
+proc listDirectories(dirPath: string): seq[string] =
+  for fd in walkDir(dirPath):
+    result.add(fd.path)
+
+proc image*(uc: ref UtliemCli): UcImage =
+  result.utliemCli = uc
+  result.imagesDirPath = uc.appDirectoryPath / "images"
 
 proc list*(i: UcImage): seq[string] =
-  @["foo", "bar"]
+  i.imagesDirPath.listDirectories
 
 proc delete*(i: UcImage, name: string) =
   discard
