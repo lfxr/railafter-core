6e31a96deac3bac5a09977439b86136aab34098b
lafixier <lfxr@proton.me>
2023-03-10 17:18:15 +0900

    feat(src/libs/errors.nim): 引数の値が1以下の場合は単数形で表示; 不要なimportを削除

--- src/libs/errors.nim
+++ src/libs/errors.nim
@@ -1,7 +1,3 @@
-import
-  strformat
-
-
 func raiseNewException(exception: typedesc, message: string) =
   raise newException(exception, message)
 
@@ -10,6 +6,9 @@ func invalidNumberOfArgs*(expectedNumberOfArgs, actualNumberOfArgs: Natural,
   raiseNewException(
     ValueError,
     "Invalid number of arguments: Command '" &
-    commandName & "' expected " & $expectedNumberOfArgs &
-    " arguments, but got " & $actualNumberOfArgs & " arguments."
+    commandName & "' expected " & $expectedNumberOfArgs & " argument" &
+    (if expectedNumberOfArgs > 1: "s" else: "") &
+    ", but got " & $actualNumberOfArgs & " argument" &
+    (if actualNumberOfArgs > 1: "s" else: "") &
+    "."
   )
