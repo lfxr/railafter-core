82bc1e0223c670a4e48edd1245848c09f2a353b7
lafixier <lfxr@proton.me>
2023-05-03 19:34:11 +0900

    fix(templates.nim): 引数filePathに変数filePathを渡さないと吐くエラーのため引数名を変更

--- src/azanautl_cli/private/templates.nim
+++ src/azanautl_cli/private/templates.nim
@@ -12,15 +12,15 @@ let yamlTemplates* = YamlTemplates()
   ## YAMLのテンプレート
 
 
-template openImageYamlFile*(filePath: string, mode: FileMode, body: untyped) =
-  let imageYamlFile = ImageYamlFile(filePath: filePath)
+template openImageYamlFile*(path: string, mode: FileMode, body: untyped) =
+  let imageYamlFile = ImageYamlFile(filePath: path)
   var imageYaml {.inject.} = imageYamlFile.load()
   try: body
   finally:
     if mode == fmWrite: discard imageYamlFile.update(imageYaml)
 
-template openContainerYamlFile*(filePath: string, mode: FileMode, body: untyped) =
-  let containerYamlFile = ContainerYamlFile(filePath: filePath)
+template openContainerYamlFile*(path: string, mode: FileMode, body: untyped) =
+  let containerYamlFile = ContainerYamlFile(filePath: path)
   var containerYaml {.inject.} = containerYamlFile.load()
   try: body
   finally:
