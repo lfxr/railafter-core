a6df297c7a023ae63cfa3f5b7ed39867d5b24170
lafixier <lfxr@proton.me>
2023-05-03 18:03:42 +0900

    refactor: YAMLファイルの読み込み・書き込みをテンプレート化

--- src/azanautl_cli/private/templates.nim
+++ src/azanautl_cli/private/templates.nim
@@ -1,5 +1,6 @@
 import
-  types
+  types,
+  yaml_file
 
 
 type YamlTemplates* = object
@@ -9,3 +10,18 @@ type YamlTemplates* = object
 
 let yamlTemplates* = YamlTemplates()
   ## YAMLのテンプレート
+
+
+template openImageYamlFile*(filePath: string, mode: FileMode, body: untyped) =
+  let imageYamlFile = ImageYamlFile(filePath: filePath)
+  var imageYaml {.inject.} = imageYamlFile.load()
+  try: body
+  finally:
+    if mode == fmWrite: discard imageYamlFile.update(imageYaml)
+
+template openContainerYamlFile*(filePath: string, mode: FileMode, body: untyped) =
+  let containerYamlFile = ContainerYamlFile(filePath: filePath)
+  var containerYaml {.inject.} = containerYamlFile.load()
+  try: body
+  finally:
+    if mode == fmWrite: discard containerYamlFile.update(containerYaml)
