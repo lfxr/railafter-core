4c40bfc744e9dafef6ea22e10ce1cfe80463cd80
lafixier <100778884+lafixier@users.noreply.github.com>
2023-05-24 18:51:33 +0900

    error: 存在しないコマンドを受け取った場合のエラーハンドリングを追加

--- src/azanautl_cli/private/cli_errors.nim
+++ src/azanautl_cli/private/cli_errors.nim
@@ -7,3 +7,6 @@ func invalidNumberOfArgsError*(
     expected, acutual: Natural
 ): string =
   fmt"コマンド'{commandName}'は{expected}個の引数を取りますが、{acutual}個の引数が与えられました"
+
+func unknownCommandError*(commandName: string): string =
+  fmt"コマンド'{commandName}'は存在しません"
