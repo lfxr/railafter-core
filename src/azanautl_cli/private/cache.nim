5e11d38b0f489ed063981d4266c74fa203a2ebd2
lafixier <100778884+lafixier@users.noreply.github.com>
2023-05-27 07:15:16 +0900

    feat(cache.nim): 基盤関係のプロシージャを追加

--- src/azanautl_cli/private/cache.nim
+++ src/azanautl_cli/private/cache.nim
@@ -19,6 +19,17 @@ type Cache* = object
   dirPath: string
 
 
+type CacheBases = object
+  cache: ref Cache
+  dirPath: string
+
+
+type CacheBasis = object
+  cache: ref Cache
+  basis: Basis
+  dirPath: string
+
+
 type CachePlugins = object
   cache: ref Cache
   dirPath: string
@@ -110,6 +121,86 @@ proc apply*(cachePlugin: CachePlugin, destDirPath: string): Result[void] =
   # ZIPファイルをコピー
   copyFile(
     cachePlugin.dirPath / UnextracedZipFileName,
+    destDirPath / UnextracedZipFileName
+  )
+
+
+func bases*(cache: ref Cache): CacheBases =
+  result.cache = cache
+  result.dirPath = cache.dirPath / "bases"
+
+
+func basis*(cache: ref Cache, basis: Basis): CacheBasis =
+  result.cache = cache
+  result.basis = basis
+  result.dirPath = cache.bases.dirPath / basis.id / basis.version
+
+
+proc cache*(
+    cacheBases: CacheBases,
+    basis: Basis,
+    zipFilePath: string
+): Result[void] =
+  ## 基盤をキャッシュする
+  result = result.typeof()()
+  let
+    packages = cacheBases.cache.packages
+    basisVersionCacheDirPath = cacheBases.dirPath / basis.id / basis.version
+    basisVersionCacheExtractedDirPath = basisVersionCacheDirPath / "extracted"
+    expectedHashValue =
+      packages.basis(basis.id).version(basis.version).sha3_512_hash
+    actualHashValue = sha3_512File(zipFilePath)
+
+  # ZIPファイルのハッシュ値を検証
+  if expectedHashValue != actualHashValue:
+    result.err = option(
+       Error(
+         kind: invalidZipFileHashValueError,
+         zipFilePath: zipFilepath,
+         expectedHashValue: expectedHashValue,
+         actualHashValue: actualHashValue
+       )
+    )
+    return
+
+  # ZIPファイルのコピー先ディレクトリを作成
+  createDir(basisVersionCacheDirPath)
+
+  # ZIPファイルをコピー
+  copyFile(zipFilePath, basisVersionCacheDirPath / "unextracted.zip")
+
+  # ZIPファイルを展開して生成されたファイル群を, 展開先ディレクトリにコピー
+  extractAll(zipFilePath, basisVersionCacheExtractedDirPath)
+
+
+func exists*(cacheBasis: CacheBasis): bool =
+  dirExists(cacheBasis.dirPath)
+
+
+proc apply*(cacheBasis: CacheBasis, destDirPath: string): Result[void] =
+  ## 基盤のキャッシュを適用する
+  let
+    packages = cacheBasis.cache.packages
+    basis = cacheBasis.basis
+    expectedHashValue =
+      packages.basis(basis.id).version(basis.version).sha3_512_hash
+    actualHashValue = sha3_512File(cacheBasis.dirPath)
+
+  # ZIPファイルのハッシュ値を検証
+  if expectedHashValue != actualHashValue:
+    result.err = option(
+       Error(
+         kind: invalidZipFileHashValueError,
+         zipFilePath: cacheBasis.dirPath,
+         expectedHashValue: expectedHashValue,
+         actualHashValue: actualHashValue
+       )
+    )
+    return
+
+  # ZIPファイルをコピー
+  copyFile(
+    cacheBasis.dirPath / UnextracedZipFileName,
     destDirPath / UnextracedZipFileName
   )
 
